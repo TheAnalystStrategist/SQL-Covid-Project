aa139e6704c9ec0a2f04c6ae1943d48132a14466
Enoch Kwame Kwakye <142117623+TheAnalystStrategist@users.noreply.github.com>
2023-08-14 20:26:56 -0400

    Update Covid projects.sql

--- Covid projects.sql
+++ Covid projects.sql
@@ -19,7 +19,6 @@ where location like '%states%'
 and continent is not null
 order by 1,2
 
-
 --look into countries with the highest infection rate compared to its population
 select location, population, MAX(total_cases) as HighestInfectionCount, max((total_cases/population))*100 as PercentPopulationInfected
 from PortfolioProject..CovidDeaths
@@ -178,3 +177,31 @@ where continent is not null
 Group by Location, population
  order by PercentPopulationInfected desc;
  
+ -- Querries for Tableau vizzies
+ --1 (Total cases vs total deaths and Death%
+ select sum(new_cases) as total_cases, sum (cast(new_deaths as int)) as total_deaths, 
+sum(cast(new_deaths as int))/sum(new_cases)*100 as DeathPercentage
+from PortfolioProject..CovidDeaths
+where continent is not null
+order by 1,2
+
+
+ -- 2 (To make the analysis consistent some locations are nulled)
+select location, sum(cast(new_deaths as int)) as TotalDeathCount
+ from PortfolioProject..CovidDeaths
+ where continent is null
+ and location not in ('world', 'European Union', 'International')
+ group by location
+ Order by TotalDeathCount desc
+
+ --3
+select location, population, MAX(total_cases) as HighestInfectionCount, max((total_cases/population))*100 as PercentPopulationInfected
+from PortfolioProject..CovidDeaths
+Group by Location, population
+order by PercentPopulationInfected desc
+
+--4
+select location, population, date, MAX(total_cases) as HighestInfectionCount, max((total_cases/population))*100 as PercentPopulationInfected
+from PortfolioProject..CovidDeaths
+Group by Location, population, date
+order by PercentPopulationInfected desc
